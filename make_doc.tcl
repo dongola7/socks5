eaa4e6614e6e40f31912cdf17731e900faeb6e07
Blair Kitchen <blair@the-blair.com>
2012-03-10 14:40:03 -0500

    Adding header comments to make_doc.tcl.

--- make_doc.tcl
+++ make_doc.tcl
@@ -2,6 +2,17 @@
 # This line continues for Tcl, but is a single line for 'sh' \
 exec tclsh "$0" ${1+"$@"}
 
+# make_doc.tcl --
+#
+#   Converts doctools formatted markup to the specified output format.
+#   Usage: ./make_doc.tcl html < socks5.man > socks5.html
+#
+# Copyright (c) 2012 Blair Kitchen
+#
+# See the file "license.terms" for information on usage and redistribution of
+# this file, and for a DISCLAIMER OF ALL WARRANTIES.
+#
+
 package require Tcl 8.5
 package require doctools 1.4
 
